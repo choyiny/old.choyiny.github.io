61e5e1ecf44491bf11b63e02fd7ba82af0c7df53
hkminegod <me@techmastary.net>
2015-12-21 17:22:45 +0800

    Update website!
    So long farewell

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,219 @@
-// global. currently active menu item 
-var current_item = 0;
+/*
+	Highlights by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-// few settings
-var section_hide_time = 400;
-var section_show_time = 800;
+(function($) {
 
-// jQuery stuff
-jQuery(document).ready(function ($) {
-    
-    //Typing
-    $(function () {
-        $(".subtitle").typed({
-            strings: ["I ♥ programming", "</i write code>", "IGN: hkminegod", "Student, Developer, Gamer"],
-            showCursor: false,
-            contentType: 'text',
-            backDelay: 1000,
-            typeSpeed: 10,
-            backSpeed: 10
-        });
-    });
-    
-	// Switch section
-	$("a", '.mainmenu').click(function () {
-		if (!$(this).hasClass('active')) {
-			current_item = this;
-			// close all visible divs with the class of .section
-			$('.section:visible').fadeOut(section_hide_time, function () {
-				$('a', '.mainmenu').removeClass('active');
-				$(current_item).addClass('active');
-				var new_section = $($(current_item).attr('href'));
-				new_section.fadeIn(section_show_time);
-			});
-		}
-		return false;
+	skel.breakpoints({
+		large: '(max-width: 1680px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
 	});
-    
-});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$html = $('html');
+
+		// Disable animations/transitions until the page has loaded.
+			$html.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$html.removeClass('is-loading');
+				}, 0);
+			});
+
+		// Touch mode.
+			if (skel.vars.mobile) {
+
+				var $wrapper;
+
+				// Create wrapper.
+					$body.wrapInner('<div id="wrapper" />');
+					$wrapper = $('#wrapper');
+
+					// Hack: iOS vh bug.
+						if (skel.vars.os == 'ios')
+							$wrapper
+								.css('margin-top', -25)
+								.css('padding-bottom', 25);
+
+					// Pass scroll event to window.
+						$wrapper.on('scroll', function() {
+							$window.trigger('scroll');
+						});
+
+				// Scrolly.
+					$window.on('load.hl_scrolly', function() {
+
+						$('.scrolly').scrolly({
+							speed: 1500,
+							parent: $wrapper,
+							pollOnce: true
+						});
+
+						$window.off('load.hl_scrolly');
+
+					});
+
+				// Enable touch mode.
+					$html.addClass('is-touch');
+
+			}
+			else {
+
+				// Scrolly.
+					$('.scrolly').scrolly({
+						speed: 1500
+					});
+
+			}
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Header.
+			var $header = $('#header'),
+				$headerTitle = $header.find('header'),
+				$headerContainer = $header.find('.container');
+
+			// Make title fixed.
+				if (!skel.vars.mobile) {
+
+					$window.on('load.hl_headerTitle', function() {
+
+						skel.on('-medium !medium', function() {
+
+							$headerTitle
+								.css('position', 'fixed')
+								.css('height', 'auto')
+								.css('top', '50%')
+								.css('left', '0')
+								.css('width', '100%')
+								.css('margin-top', ($headerTitle.outerHeight() / -2));
+
+						});
+
+						skel.on('+medium', function() {
+
+							$headerTitle
+								.css('position', '')
+								.css('height', '')
+								.css('top', '')
+								.css('left', '')
+								.css('width', '')
+								.css('margin-top', '');
+
+						});
+
+						$window.off('load.hl_headerTitle');
+
+					});
+
+				}
+
+			// Scrollex.
+				skel.on('-small !small', function() {
+					$header.scrollex({
+						terminate: function() {
+
+							$headerTitle.css('opacity', '');
+
+						},
+						scroll: function(progress) {
+
+							// Fade out title as user scrolls down.
+								if (progress > 0.5)
+									x = 1 - progress;
+								else
+									x = progress;
+
+								$headerTitle.css('opacity', Math.max(0, Math.min(1, x * 2)));
+
+						}
+					});
+				});
+
+				skel.on('+small', function() {
+
+					$header.unscrollex();
+
+				});
+
+		// Main sections.
+			$('.main').each(function() {
+
+				var $this = $(this),
+					$primaryImg = $this.find('.image.primary > img'),
+					$bg,
+					options;
+
+				// No primary image? Bail.
+					if ($primaryImg.length == 0)
+						return;
+
+				// Hack: IE8 fallback.
+					if (skel.vars.IEVersion < 9) {
+
+						$this
+							.css('background-image', 'url("' + $primaryImg.attr('src') + '")')
+							.css('-ms-behavior', 'url("css/ie/backgroundsize.min.htc")');
+
+						return;
+
+					}
+
+				// Create bg and append it to body.
+					$bg = $('<div class="main-bg" id="' + $this.attr('id') + '-bg"></div>')
+						.css('background-image', (
+							'url("css/images/overlay.png"), url("' + $primaryImg.attr('src') + '")'
+						))
+						.appendTo($body);
+
+				// Scrollex.
+					options = {
+						mode: 'middle',
+						delay: 200,
+						top: '-10vh',
+						bottom: '-10vh'
+					};
+
+					if (skel.canUse('transition')) {
+
+						options.init = function() { $bg.removeClass('active'); };
+						options.enter = function() { $bg.addClass('active'); };
+						options.leave = function() { $bg.removeClass('active'); };
+
+					}
+					else {
+
+						$bg
+							.css('opacity', 1)
+							.hide();
+
+						options.init = function() { $bg.fadeOut(0); };
+						options.enter = function() { $bg.fadeIn(400); };
+						options.leave = function() { $bg.fadeOut(400); };
+
+					}
+
+					$this.scrollex(options);
+
+			});
+
+	});
+
+})(jQuery);
